dda5aea9aafbe5542ed423f4741730bcc8b2f8b0
GroupWeb21 <80159815+ThanhNghia-JV4@users.noreply.github.com>
2023-03-24 08:18:03 +0700

    NEW

--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
+const authRouter = require('./routes/auth');
+
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect(`mongodb+srv://mern-learn:1234@mern-learn.dmjpofx.mongodb.net/mern-learn?retryWrites=true&w=majority`)
-		console.log('MongoDB connected')
+		await mongoose.connect(`mongodb+srv://mern-learn:1234@mern-learn.dmjpofx.mongodb.net/mern-learn?retryWrites=true&w=majority`);
+		console.log('MongoDB connected');
 	} catch (error) {
-		console.log(error.message)
-		process.exit(1)
+		console.log(error.message);
+		process.exit(1);
 	}
 }
 
-connectDB()
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello '));
-
+app.use('/api/auth', authRouter);
 const PORT = 5000;
 
 app.listen(PORT, () => console.log('server listening on port ' + PORT));
